75d7a6a8b9d3e76faa6edf63fde1f421c34cbdbc
Sigit Wahyudi <wahyudi.sigit@gmail.com>
2020-08-30 16:27:01 +0800

    up3

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
-# If any part of the script fails the deploy stops 
+
+# If a command fails then the deploy stops
 set -e
 
-# Status message
-printf "\033[0;32mDeploying updates to GitHub…\033[0m\n" 
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project. 
-hugo
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder 
-cd public 
+# Go To Public folder
+cd public
 
-# Add changes to git. 
-git add . 
+# Add changes to git.
+git add .
 
-# Commit changes. 
-msg="Deploying site $(date)" if [ -n "$*" ]; then msg="$*" fi git commit -m "$msg" 
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
 
-# Push source and deploy. 
+# Push source and build repos.
 git push origin master
